7acd632cdae71245ac06ad77acae1ed57461b9d3
CodingWith-Adam <adam@example.com>
2020-08-30 17:05:57 -0600

    Updates

---
+++ precache-manifest.282af8f3005ae9458809352858103b9a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ff9ff19b74d2ee23d505b93bdcbe03c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1eff8bf1caea5963c025",
+    "url": "/static/css/2.28a6ffea.chunk.css"
+  },
+  {
+    "revision": "ea13beb1fbdc444c8642",
+    "url": "/static/css/main.ac76e5f0.chunk.css"
+  },
+  {
+    "revision": "1eff8bf1caea5963c025",
+    "url": "/static/js/2.0972dd09.chunk.js"
+  },
+  {
+    "revision": "47beb0510b7c1e46fd9850a65b52e1ac",
+    "url": "/static/js/2.0972dd09.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ea13beb1fbdc444c8642",
+    "url": "/static/js/main.c7363ee2.chunk.js"
+  },
+  {
+    "revision": "8814150e2adf1f77ff39",
+    "url": "/static/js/runtime-main.64585b87.js"
+  }
+]);
